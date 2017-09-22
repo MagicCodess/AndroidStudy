5eff2fb770a0dcc2e77fd4bc7ebb67a9ccb7d9ef
MagicCodess <1009719507@qq.com>
2017-09-22 11:16:39 +0800

    MiniWeather

---
+++ app/src/main/java/cn/edu/pku/zhangchenning/miniweather/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.edu.pku.zhangchenning.miniweather;
+import android.app.Activity;
+import android.os.Bundle;
+/**
+ * Created by Arthur on 2017/9/22.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
